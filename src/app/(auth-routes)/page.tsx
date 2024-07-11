8831e0ef74e0fe73b55ec3418fa7466b3b4c2163
Alef Gaigher <alef@adaline.com.br>
2024-07-11 12:18:18 -0300

    carregamento

--- src/app/(auth-routes)/page.tsx
+++ src/app/(auth-routes)/page.tsx
@@ -5,32 +5,37 @@ import { useRouter } from "next/navigation";
 import { SyntheticEvent, useState } from "react";
 
 export default function Home() {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');  
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter()
 
   async function handleSubmit(event: SyntheticEvent) {
     event.preventDefault()
+    
+    try {
+      setIsLoading(true);
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false
+      });
 
-    const result = await signIn('credentials', {
-      email,
-      password,
-      redirect: false
-    })
-
-    if (result?.error) {
-      console.log(result)
-      return
+    }catch(error: any){
+      setIsLoading(false);
+      setError(error.message);
+    }finally{
+      setIsLoading(false);
+      router.replace('/admin');
     }
-
-    router.replace('/admin')
   }
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
       <h1 className="text-3xl mb-6">Login</h1>
-
+      {error && <p className="mb-4 text-red-600">{error}</p>}
       <form className="w-[400px] flex flex-col gap-6" onSubmit={handleSubmit}>
         <input 
           className="h-12 rounded-md p-2 bg-transparent border border-gray-300"
@@ -38,6 +43,7 @@ export default function Home() {
           name="email" 
           placeholder="Digite seu e-mail" 
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
 
         <input 
@@ -46,13 +52,22 @@ export default function Home() {
           name="password" 
           placeholder="Digite sua senha" 
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
 
-        <button
-          type="submit"
-          className="h-12 rounded-md bg-gray-300 text-gray-800 hover:bg-gray-400"
-        >
-          Entrar
+        
+        <button type="submit" className="h-12 rounded-md bg-gray-300 text-gray-800 hover:bg-gray-400 flex items-center justify-center" disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <svg className="animate-spin h-5 w-5 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.964 7.964 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+              Entrando...
+            </>
+          ) : (
+            'Entrar'
+          )}
         </button>
       </form>
       <div className="mt-4 text-lg text-center">
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -14,6 +14,7 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [phone, setPhone] = useState('');
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleBack = () => {
     router.back();
@@ -21,10 +22,14 @@ const Register = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
     if (password !== confirmPassword) {
       setError('Passwords do not match');
       return;
     }
+
+    setIsLoading(true);
+
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
@@ -32,8 +37,6 @@ const Register = () => {
       await updateProfile(user, {
         displayName: name  
       });
-
-      
 
       await setDoc(doc(db, 'users', user.uid), {
         name,
@@ -43,76 +46,94 @@ const Register = () => {
 
       router.push('/');
     } catch (error: any) {
+      setIsLoading(false);
       setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
   
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-800">
-      <div className="p-6 border border-gray-300 rounded-lg shadow-lg w-7/12">
-        <h1 className="mb-6 text-2xl font-bold text-center">Register</h1>
+      <div className="p-6 w-5/12">
+        <h1 className="mb-6 text-2xl font-bold text-center">Cadastrar</h1>
         {error && <p className="mb-4 text-red-600">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block mb-2">Nome</label>
             <input
               type="text"
               value={name}
+              placeholder="Digite seu nome" 
               onChange={(e) => setName(e.target.value)}
               className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
               required
+              disabled={isLoading}
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Email</label>
             <input
               type="email"
               value={email}
+              placeholder="Digite seu e-mail" 
               onChange={(e) => setEmail(e.target.value)}
               className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
               required
+              disabled={isLoading}
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Senha</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Confirmar Senha</label>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Telefone</label>
             <input
               type="tel"
               value={phone}
+              placeholder="Digite seu telefone" 
               onChange={(e) => setPhone(e.target.value)}
               className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
               required
+              disabled={isLoading}
             />
           </div>
-          <div className='grid grid-cols-2 gap-5'>
+          <div className="mb-4">
+            <input
+              type="password"
+              value={password}
+              placeholder="Digite sua senha" 
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
+              required
+              disabled={isLoading}
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              value={confirmPassword}
+              placeholder="Repita sua senha" 
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-lg bg-transparent border-gray-300"
+              required
+              disabled={isLoading}
+            />
+          </div>
+          
+          <div className='grid grid-cols-2 gap-5 mt-10'>
             <button type="button" onClick={handleBack} className="w-full px-4 py-2 font-bold rounded-lg h-12  bg-blue-500 text-white hover:bg-blue-600">
                 Voltar
             </button>
-            <button type="submit" className="w-full px-4 py-2 font-bold rounded-lg h-12  bg-gray-300 text-gray-800 hover:bg-gray-400">
-                Salvar
+            <button type="submit" className="w-full px-4 py-2 font-bold rounded-lg h-12 bg-gray-300 text-gray-800 hover:bg-gray-400 flex items-center justify-center" disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  <svg className="animate-spin h-5 w-5 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.964 7.964 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Salvando...
+                </>
+              ) : (
+                'Salvar'
+              )}
             </button>
-          </div>
-          
+          </div>   
         </form>
       </div>
     </div>
